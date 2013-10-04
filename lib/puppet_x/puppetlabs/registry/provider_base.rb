a88cc924661ea1e5909feb757ec715851f4219c7
Mark Myers <marcusmyers@gmail.com>
2013-10-04 13:44:15 -0400

    Added hkcu to use with windows machines

--- lib/puppet_x/puppetlabs/registry/provider_base.rb
+++ lib/puppet_x/puppetlabs/registry/provider_base.rb
@@ -33,6 +33,7 @@ module ProviderBase
           :hkcr => Win32::Registry::HKEY_CLASSES_ROOT,
           :hklm => Win32::Registry::HKEY_LOCAL_MACHINE,
           :hkcc => Win32::Registry::HKEY_CURRENT_CONFIG,
+	  :hkcu => Win32::Registry::HKEY_CURRENT_USER,
         }
       end
     end
